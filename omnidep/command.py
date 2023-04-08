069c6fb2911effc7a91c75a847daab785acf1933
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2023-04-08 13:53:16 +0100

    Just define type-checkers once

--- omnidep/command.py
+++ omnidep/command.py
@@ -82,33 +82,14 @@ class Config:
             # supports them.
             if key not in allowed:
                 raise ConfigError(f"Config option {orig_key!r} not recognised")
-            # Try to give a useful error message for misconfigurations.
-            # Not many types needed, so just hack them rather than add a
-            # dependency on pydantic.
-            check: Dict[str, Callable[[object], bool]]
-            check = {
-                'bool': lambda val: isinstance(val, bool),
-                'List[str]': lambda val: (
-                    isinstance(val, list) and
-                    all(isinstance(x, str) for x in val)
-                ),
-                'List[Path]': lambda val: (
-                    isinstance(val, list) and
-                    all(isinstance(x, str) for x in val)
-                ),
-                'Dict[str, List[str]]': lambda val: (
-                    isinstance(val, dict) and
-                    all(isinstance(key, str) for key in val) and
-                    all(map(check['List[str]'], val.values()))
-                ),
-            }
             convert: Dict[str, Callable[[Any], Any]] = {
                 'List[Path]': lambda val: list(map(Path, val))
             }
             # It already is a str due to "import annotations". But mypy doesn't
             # seem to know that, so coerce it.
             expected_type = str(allowed[key].type)
-            if not check[expected_type](value):
+            # Try to give a useful error message for misconfigurations.
+            if not check_type[expected_type](value):
                 raise ConfigError(f"Config option {orig_key!r}: expected {expected_type}, got {value!r}")
             converter = convert.get(expected_type, lambda x: x)
             args[key] = converter(value)
@@ -116,3 +97,23 @@ class Config:
             new_paths = [toml_file.parent / path for path in args['local_test_paths']]
             args['local_test_paths'] = new_paths
         return Config(**args)
+
+# Not many types needed, so just hack them rather than add a
+# dependency on pydantic.
+check_type: Dict[str, Callable[[object], bool]]
+check_type = {
+    'bool': lambda val: isinstance(val, bool),
+    'List[str]': lambda val: (
+        isinstance(val, list) and
+        all(isinstance(x, str) for x in val)
+    ),
+    'List[Path]': lambda val: (
+        isinstance(val, list) and
+        all(isinstance(x, str) for x in val)
+    ),
+    'Dict[str, List[str]]': lambda val: (
+        isinstance(val, dict) and
+        all(isinstance(key, str) for key in val) and
+        all(map(check_type['List[str]'], val.values()))
+    ),
+}
