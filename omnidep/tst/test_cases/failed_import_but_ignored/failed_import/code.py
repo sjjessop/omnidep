43e6b09968b704033667d9b95d06d663f095763a
Steve Jessop <68118527+sjjessop@users.noreply.github.com>
2024-04-13 15:40:05 +0100

    Enable ruff rules to check for blanket lint ignore

--- omnidep/tst/test_cases/failed_import_but_ignored/failed_import/code.py
+++ omnidep/tst/test_cases/failed_import_but_ignored/failed_import/code.py
@@ -1,4 +1,4 @@
 
 # Import something not installed locally: will provoke error as long as this
 # file is actually checked
-import no_such_module  # type: ignore  # noqa: F401
+import no_such_module  # type: ignore[import-not-found]  # noqa: F401
--- omnidep/tst/test_cases/parent_child_misconfigured/parent_child/code.py
+++ omnidep/tst/test_cases/parent_child_misconfigured/parent_child/code.py
@@ -2,4 +2,4 @@
 # Import where we declare a dependency on a parent, not on this package.
 import coverage  # noqa: F401
 # TODO - shouldn't need this, but it prevents Violation.ODEP005: 'unused-dependency'
-import pytest_cov  # type: ignore  # noqa: F401
+import pytest_cov  # type: ignore[import-untyped]  # noqa: F401
